cbe3abf897c4b7d073bb73528bc3f17edd97f0c7
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-07-22 20:32:59 -0500

    Header goofy ah

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <h1>hihjhbhbjhbhjbhjbjhbhb</h1>;
+  return <></>;
 };
 
 export default Home;
